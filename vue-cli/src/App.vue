438e09957a36fd84b00e0cd7df2884342ea31b38
Mikkel <Mikkelmbk@users.noreply.github.com>
2020-07-14 15:03:52 +0200

    vue local vs global components

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <my-component></my-component>  <!-- comming from the globally registered component in main.js -->
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <app-server-status v-for="(server, index) in 5" :key="index"></app-server-status> <!-- comming from locally registered component in ServerStatus.vue -->
+  </div>
+</template>
+
+<script>
+import ServerStatus from "./ServerStatus"; // locally registered component
+export default {
+  components: {
+    "app-server-status": ServerStatus
+  }
+};
+</script>
